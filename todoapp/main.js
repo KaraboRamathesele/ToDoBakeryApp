de8731698f227409d5c66f8742a1dafab4a232ce
KaraboRamathesele <karaboramathesele8115@gmail.com>
2022-09-09 16:36:26 +0200

    add background image and javascript import

---
+++ todoapp/main.js
@@ -0,0 +1,99 @@
+
+let form = document.getElementById('form');
+let textInput = document.getElementById('textInput');
+let dateInput = document.getElementById('dateInput');
+let textarea = document.getElementById('textarea');
+let msg = document.getElementById('msg');
+let tasks = document.getElementById('tasks');
+let addBtn = document.getElementById('addBtn');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    formValidation();
+});
+
+let formValidation = () => {
+    if (textInput.value === '') {
+        console.log('blank form');
+        msg.innerHTML = 'Task cannot be blank'; 
+    } else {
+        console.log('valid form');
+        msg.innerHTML = '';
+        acceptData();
+        addBtn.setAttribute('data-bs-dismiss','modal');
+        addBtn.click();
+
+       (() => {
+        addBtn.setAttribute('data-bs-dismiss','');
+       })();
+    }
+};
+
+let data = [];
+
+// Use this function to fetch data in input and store in empty object. 
+let acceptData = () => {
+    data.push({
+    text: textInput.value,
+    date: dateInput.value,
+    description: textarea.value
+    });
+
+    localStorage.setItem('data', JSON.stringify(data));
+
+    createTasks();
+    // console.log(data);
+};
+
+// Output the result inputed from modal on list
+let createTasks = () => {
+    tasks.innerHTML = '';
+    data.map((x,y) => {
+        return (tasks.innerHTML += `
+        <div id=${y}>
+        <span class="fw-bold">${x.text}</span>
+        <span class="small text-secondary">${x.date}</span>
+    
+        <p>${x.description}</p>
+        <span class="options">
+          <i onClick = 'editTask(this)' data-bs-toggle="modal" data-bs-target="#form" class="fa-solid fa-pen-to-square"></i>
+          <i onClick = 'deleteTask(this);createTasks()' class="fa-solid fa-trash"></i>
+        </span>
+      </div>
+      `);
+    });
+
+    
+
+  resetForm();
+
+};
+
+let resetForm = ()=> {
+    textInput.value = '';
+    dateInput.value = '';
+    textarea.value = '';
+};
+
+let deleteTask = (e) => {
+    e.parentElement.parentElement.remove();
+    data.splice(e.parentElement.parentElement.id, 1);
+    localStorage.setItem('data', JSON.stringify(data));
+}
+
+// 
+let editTask = (e)=> {
+    let selectedTask =  e.parentElement.parentElement; // div with tasks is the main parent 
+
+    textInput.value = selectedTask.children[0].innerHTML
+    dateInput.value = selectedTask.children[1].innerHTML
+    textarea.value = selectedTask.children[2].innerHTML
+
+    deleteTask(e);
+}
+
+(() => {
+    data = JSON.parse(localStorage.getItem('data')) || [];
+    console.log(data);
+    createTasks();
+})()
